0a4a3e3e68021d3a36248dc8bd76c188ba4981fc
gonci <gonci@LAPTOP-J6NKDN0B>
2022-10-14 17:32:55 +0200

    Añadiendo prints en main - 4.1

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -3,8 +3,10 @@ package demo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola ");
+		System.out.println("Hola ");
+		System.out.println("Hola ");
+		
 	}
 
 }
